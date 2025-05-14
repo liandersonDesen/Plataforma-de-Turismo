4989b8ca0c17c20de49c57eab8202c847aeb0e8a
Lianderson <liandersonr18@gmail.com>
2025-05-14 13:09:44 -0300

    cria o middleware e schemas para validar os dados do usuario

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { LoginUser, RegisterUser } from "../controllers/UserControllers.js";
+import { validate } from "../middleware/validate.js";
+import { LoginUserSchema, RegisterUserSchema } from "../schemas/UserSchemas.js";
 
 const router = express.Router()
 
-router.post('/register',RegisterUser)
-router.post('/login',LoginUser)
+router.post('/register',validate(RegisterUserSchema),RegisterUser)
+router.post('/login', validate(LoginUserSchema),LoginUser)
 
 
 export default router
