d88938f3a9bd851a4e7ec5e9aac903f33dd44716
Lianderson <liandersonr18@gmail.com>
2025-05-17 00:31:02 -0300

    cria a autenticação dos usuários

--- src/routes/PlaceRoutes.js
+++ src/routes/PlaceRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 import { CreatePlace, DeletePlace, GetPlaces, UpdatePlace } from "../controllers/PlaceController.js";
 import { validate } from "../middleware/validate.js";
 import { CreatePlaceSchema, UpdatePlaceSchema } from "../schemas/PlaceSchemas.js";
+import { authhenticateToken, authorization } from "../utils/auth.js";
 
 const router=express.Router()
 
-router.post("/",validate(CreatePlaceSchema),CreatePlace)
-router.get("/",GetPlaces)
-router.put("/:id",validate(UpdatePlaceSchema),UpdatePlace)
-router.delete("/:id",DeletePlace)
+router.post("/",authhenticateToken(),authorization(['ADMIN']),validate(CreatePlaceSchema),CreatePlace)
+router.get("/",authhenticateToken(),authorization(['ADMIN','USER']),GetPlaces)
+router.put("/:id",authhenticateToken(),authorization(['ADMIN']),validate(UpdatePlaceSchema),UpdatePlace)
+router.delete("/:id",authhenticateToken(),authorization(['ADMIN']),DeletePlace)
 
 export default router
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -27,3 +27,46 @@ export function gerarToken(user) {
     
 }
 
+export function authhenticateToken() {
+    try {
+    
+    return (req, res, next) => {
+        const authHeader = req.headers['authorization'];
+        
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if (token == null) {
+            return res.status(401).send("Não há Token");
+        }
+
+        jwt.verify(token, jwt_secret, (err, user) => {
+            if (err) {
+                return res.status(403).send("Token inválido");
+            }
+            req.user = user;
+            next();
+        })
+
+    }
+    
+        
+    } catch (error) {
+        
+    return res.status(400).json({
+        messagem:"Erro de autenticação",
+        erro:error
+        })
+
+    }
+    
+}
+export function authorization(roles){
+    return (req,res,next) => {
+            if(req.user && roles.includes(req.user.role)){
+                next();
+            }else{
+                return res.status(403).send("Acesso negado");
+            }
+        
+    }
+}
